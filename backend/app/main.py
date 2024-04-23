55be3d17478447408021ee1f8cee22c79be8908d
Diego Santodomingo <diegosantodomingo26@gmail.com>
2024-04-23 20:46:45 +0200

    fixed commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 from fastapi import  FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import users
 from . import models
-=======
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
-from fastapi.middleware.cors import CORSMiddleware
-
-from . import crud, models, schemas
->>>>>>> 7e3301170ef87d59b74b5bb32c034944418efce1
 from .database import SessionLocal, engine
 
 
@@ -19,13 +11,10 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-<<<<<<< HEAD
 ##Routers
 app.include_router(users.router)
 
 
-=======
->>>>>>> 7e3301170ef87d59b74b5bb32c034944418efce1
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
